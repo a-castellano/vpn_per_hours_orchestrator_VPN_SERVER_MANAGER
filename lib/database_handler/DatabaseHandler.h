28effcf1e4e28234c23d0860807f0bbcd50219d9
Azas <azas@Enki.windmaker.net>
2016-10-24 00:19:43 +0200

    vector

--- lib/database_handler/DatabaseHandler.h
+++ lib/database_handler/DatabaseHandler.h
@@ -4,6 +4,9 @@
 #ifndef DATABASEHANDLER
 #define DATABASEHANDLER
 
+#include <vector>
+#include <iostream>
+
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/prepared_statement.h>
